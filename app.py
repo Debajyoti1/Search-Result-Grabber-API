5a409afdcd239c28eee3a4dc04d9634a36389d48
Debajyoti Dutta <debajyotidutta.50@gmail.com>
2020-08-17 12:34:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
-
 app = Flask(__name__)
 app.secret_key='Hellothere'
 
