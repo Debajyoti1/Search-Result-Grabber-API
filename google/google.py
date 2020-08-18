374998e390d309c65fc7e916a5524b131725ef25
Debajyoti Dutta <debajyotidutta.50@gmail.com>
2020-08-18 16:09:59 +0530

    test

--- google/google.py
+++ google/google.py
@@ -1,16 +1,12 @@
 def Google(c,q):
     from selenium import webdriver
     from bs4 import BeautifulSoup
-    from fake_useragent import UserAgent
     import os
-    ua = UserAgent()
-    usrand=ua.random
     chrome_options = webdriver.ChromeOptions()
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("user-agent="+usrand)
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     driver.get('https://google.co.in/search?q='+q)
     source=driver.page_source
